7ae44882773ea0caf090325445f156564df9afd7
maya.uno <mayahikick@gmail.com>
2021-12-19 15:35:24 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,30 @@
+class Mentor
+   #インスタンス変数
+   attr_accessor :name
+   
+   def initialize(name)
+       self.name = name
+   end
+   
+   def job
+       puts "#{self.name}です。私は現役のITプロフィッショナルです。"
+   end
+end
+       
+class RailsMentor < Mentor
+    
+    def initialize(name)
+        self.name = name
+    end
+    
+    def job
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+end
+    
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+    
+kirameki.job
+akaide.job
+    
